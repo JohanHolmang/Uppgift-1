bea8205fd0928a4127475ed311f1d3c1cc309c09
JohanHolmang <johan.holmang@hotmail.com>
2019-08-15 13:53:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let input = document.getElementById('name');
+let button = document.getElementById('button');
+
+button.addEventListener('click', onButtonClick);
+
+onButtonClick(10, 5, 2)
+
+function onButtonClick(Fucker, Bastard, Dipshit) {
+    alert('Hello ' + input.value)
+}
